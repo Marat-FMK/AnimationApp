9c0097c9256f91db99c07c0a4f50f98f0f303d3f
MaratXcode <kosjachok8@yandex.ru>
2024-07-04 17:07:31 +0300

    work with inner view

--- AnimationApp.V6/SpringAnimation.swift
+++ AnimationApp.V6/SpringAnimation.swift
@@ -9,19 +9,19 @@ import Foundation
 import SpringAnimation
 
 struct Animation {
-    let preset: String
-    let curve: String
+    let preset: AnimationPreset
+    let curve: AnimationCurve
     let force: Double
     let duration: Double
     let delay: Double
     
     static func getAnimation() -> [Animation] {
         let animations =  [
-            Animation(preset: "squeezeDown", curve: "linear", force: 1, duration: 3, delay: 0),
-            Animation(preset: "flash", curve: "easeIn", force: 1, duration: 3, delay: 0),
-            Animation(preset: "morph", curve: "easeInOutExpo", force: 1, duration: 3, delay: 0),
-            Animation(preset: "flipX", curve: "easeInCubic", force: 1, duration: 3, delay: 0),
-            Animation(preset: "swing", curve: "easeToExpo", force: 1, duration: 3, delay: 0)
+            Animation(preset: .flash, curve:.easeInOutBack , force: 1, duration: 3, delay: 0),
+            Animation(preset: .fadeInRight, curve: .easeInOutExpo, force: 1, duration: 3, delay: 0),
+            Animation(preset: .zoomIn, curve: .easeOutQuart, force: 1, duration: 5, delay: 0),
+            Animation(preset: .squeezeUp, curve: .easeOutSine, force: 1, duration: 5, delay: 0),
+            Animation(preset: .slideLeft, curve: .easeInQuad, force: 1, duration: 3, delay: 0)
      ]
         return animations
             }
--- AnimationApp.V6/ViewController.swift
+++ AnimationApp.V6/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var animationView: SpringView!
     
+    @IBOutlet weak var innerView: SpringLabel!
     @IBOutlet weak var runAnimateSpringButton: SpringButton!
     @IBOutlet weak var forceLabel: UILabel!
     @IBOutlet weak var curveLabel: UILabel!
@@ -21,7 +22,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // runSpringAnimation(runAnimateSpringButton) // run this method if you need to set the values immediatel
     }
     
     var index = 0
@@ -38,19 +39,27 @@ class ViewController: UIViewController {
         
         let nextAnimation = animations[index]
         
-        sender.setTitle("Next - \(nextAnimation.preset)", for: .normal)
-
+        sender.setTitle("Next - \(nextAnimation.preset.rawValue)", for: .normal)
+        
+        sender.force = nextAnimation.force
+        sender.curve = nextAnimation.curve.rawValue
+        sender.animation = nextAnimation.preset.rawValue
+        sender.animate()
+        
+        innerView.force = nextAnimation.force
+        innerView.curve = nextAnimation.curve.rawValue
+        innerView.animation = nextAnimation.preset.rawValue
+        innerView.delay = 3
+        innerView.animate()
+        
         animationView.force = currentAnimation.force
-        animationView.curve = currentAnimation.curve
-        animationView.animation = currentAnimation.preset
+        animationView.curve = currentAnimation.curve.rawValue
+        animationView.animation = currentAnimation.preset.rawValue
         animationView.animate()
         
         forceLabel.text = "\(currentAnimation.force)"
-        curveLabel.text = currentAnimation.curve
-        presentLabel.text = currentAnimation.preset
-        
+        curveLabel.text = currentAnimation.curve.rawValue
+        presentLabel.text = currentAnimation.preset.rawValue
     }
-    
-
 }
 
